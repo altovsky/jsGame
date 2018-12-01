25e186e054edbacb27b93c118df1654aa1a7de20
altovsky <41269058+altovsky@users.noreply.github.com>
2018-12-01 03:53:33 +0300

    p2

--- game.js
+++ game.js
@@ -64,8 +64,90 @@ class Actor {
       return false;
     }
     
-    return this.left < actor.right && this.right > actor.left && this.top < actor.bottom && this.bottom > actor.top ? true: false;
+    return this.left < actor.right && this.right > actor.left && this.top < actor.bottom && this.bottom > actor.top;
   }
 
 }
 
+
+class Level {
+  constructor(grid = [], actors = []) {
+    this.grid = grid;
+    this.actors = actors;
+    actors.some((actor) => {
+      if (actor.type === 'player') {
+        this.player = actor;
+        return true;
+      }
+    })
+    this.status = null;
+    this.height = this.grid.length;
+    this.width = this.grid.reduce((maxLength, element) => {
+      return maxLength < element.length ? element.length: maxLength;
+    }, 0);
+    this.finishDelay = 1;
+  }
+
+  isFinished() {
+    return this.status !== null && this.finishDelay < 0;
+  }
+
+  actorAt(actor) {
+    if (!(actor) || (!(actor instanceof Actor))) {
+      throw new Error('Не передан аргумент или передан не объект Actor')
+    }
+    return this.actors.find((element) => {
+      return actor.isIntersect(element);
+    })
+  }
+
+  obstacleAt(place, size) {
+    if (!(place instanceof Vector && size instanceof Vector)) {
+      throw new Error('Переданы аргументы, которые не являются объектами Vector');
+    }
+
+    let check = new Actor(place, size);
+    if (check.bottom > this.height) {
+      return 'lava';
+    } else if (check.top < 0 || check.left < 0 || check.right > this.width) {
+      return 'wall';
+    } else {
+      for (let y = Math.floor(check.top); y < check.bottom; ++y) {
+        for (let x = Math.floor(check.left); x < check.right; ++x) {
+          if (this.grid[y][x] !== undefined) {
+            return this.grid[y][x];
+          }
+        }
+      }
+    }
+  }
+
+  removeActor(actor) {
+    this.actors.forEach((element, index) => {
+      if (actor === element) {
+        this.actors.splice(index, 1);
+      }
+    })
+  }
+
+  noMoreActors(actorType) {
+    return (!(this.actors.some((element) => {
+      return element.type === actorType;
+    })))
+  }
+
+  playerTouched(objectType, actor) {
+    if (this.status !== null) {
+      return;
+    }
+
+    if (objectType === 'lava' || objectType === 'fireball') {
+      this.status = 'lost';
+    } else if (objectType === 'coin') {
+      this.removeActor(actor);
+      if (this.noMoreActors(objectType)) {
+        this.status = 'won';
+      }
+    } 
+  }
+}
